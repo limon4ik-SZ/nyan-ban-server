e5fa05c30ecca3a6be3d38a18953ab2581762550
limon4ik-SZ <limonv4ai@gmail.com>
2026-06-08 01:36:52 +0400

    добавил серверв
    вввввввв

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>🌈 NYAN CAT ONLINE! 🌈</h1>
+        <p>Нажми <strong>пробел</strong> чтобы прыгать!</p>
+        <canvas id="gameCanvas" width="800" height="250"></canvas>
+        <script>
+            // Твой код игры здесь
+            console.log("Nyan Cat готов!");
+        </script>
+    `);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(\`Сервер работает на порту \${PORT}\`);
+});
